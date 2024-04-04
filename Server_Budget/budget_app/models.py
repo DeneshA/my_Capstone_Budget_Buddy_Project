90f8d9124e7ef2fff53a1c4752507508f3e8de37
Denesh Anandathasan <denesh555@gmail.com>
2024-04-04 11:16:01 -0400

    Income model created completed all backend codes

--- Server_Budget/budget_app/models.py
+++ Server_Budget/budget_app/models.py
@@ -11,3 +11,18 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+class Income(models.Model):
+    PAY_TERMS = [('Daily', 'Daily'), ('Weekly', 'Weekly'), ('Bi-Weekly', 'Bi-Weekly'), ('Monthly', 'Monthly'), ('Annually', 'Annually')]
+    DURATION_TERM = [('Month', 'Month'), ('Year', 'Year')]
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)  # using Django's built-in User model
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='incomes')
+    duration = models.IntegerField()
+    duration_terms = models.CharField(max_length=20, choices=DURATION_TERM)
+    start_date = models.DateField(null=True,blank=True)
+    end_date = models.DateField(null=True,blank=True)
+    bill_amount = models.FloatField()
+    bill_cycle = models.CharField(max_length=20, choices=PAY_TERMS)
+    notes = models.CharField(max_length=250,blank=True)
+    
+    def __str__(self):
+        return f"{self.category_id.category_name} - {self.bill_amount} ({self.user_id.username})"
